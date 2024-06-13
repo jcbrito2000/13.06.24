526aa781a2e21c7b0364efc0601f7f720cfd90a4
jcbrito2000 <protocoloz39.50@gmail.com>
2024-06-12 22:30:13 -0300

    Add files via upload

---
+++ script_inserir_registro.sql
@@ -0,0 +1,1 @@
+INSERT INTO `db_estudos`.`tbl_livros` (`sobrenome_autor`, `nome_autor`, `nome_do_livro`, `cidade_editora`, `nome_da_editora`) VALUES ('coelho', 'paulo', 'o alquimista', 'sao paulo', 'paralela');
